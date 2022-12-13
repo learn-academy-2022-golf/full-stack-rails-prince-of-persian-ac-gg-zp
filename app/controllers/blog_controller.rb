ab488bb1e4489a7ed5b403970c25e04e3449637b
annabellachan <annabellac24@gmail.com>
2022-12-12 16:31:42 -0800

    completed create and delete; working on edit and update

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -21,6 +21,19 @@ class BlogController < ApplicationController
           end
         end
       end
+    def destroy
+        @blog = Blog.find(params[:id])
+        if @blog.destroy    
+            redirect_to blogs_path
+        end
+    end
+    def edit
+        @blog = Blog.find(params[:id])
+    end
+    def update
+        @blog = Blog.find(params[:id])
+        @blog.update(blog_params)
+    end
 private 
     def blog_params
         params.require(:blog).permit(:title, :content)
